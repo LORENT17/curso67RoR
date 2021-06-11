8dc3891c3ee3cf5c4e5fedb84f274c0a662d438c
lorent santiago <lorentsantiago10@gmail.com>
2021-06-11 19:50:12 -0400

    algoritmosV

--- JavaScript/ejerciciosresueltos.js
+++ JavaScript/ejerciciosresueltos.js
@@ -1,7 +1,9 @@
-/*3-Dado un array de números, crea una función que dé como resultado un nuevo array donde los valores negativos se 
-reemplacen por el texto (string) ‘Dojo’. Por ejemplo, reemplazarNegativos([1,2,-3,-5,5]) debiera devolver [1,2, “Dojo”, “Dojo”, 5].*/
-
-
+/*1-Dados un array y un valor Y, cuenta e imprime (print) el número de valores del array que sean mayores que Y. 
+2-Dado un array, imprime los valores máximos (max), mínimos (min) y promedio (average) para el array. 
+3-Dado un array de números, crea una función que dé como resultado un nuevo array donde los valores negativos se 
+reemplacen por el texto (string) ‘Dojo’. Por ejemplo, reemplazarNegativos([1,2,-3,-5,5]) debiera devolver [1,2, “Dojo”, “Dojo”, 5].
+4-Dado un array y su respectivo índice, remueve los valores en el rango del índice dado( acortando el array). 
+Por ejemplo, removerRango([20,30,40,50,60,70],2,4)debiera devolver [20,30,70].*/
 
 /*---------------Ejercicio numero 3--------------------------------------------------------------------------------*/
 
@@ -15,15 +17,12 @@ function reemplazarNegativos(x) {
 }
 console.log(reemplazarNegativos([1, 2, -3, -5, 5]))
 
-/*4-Dado un array y su respectivo índice, remueve los valores en el rango del índice dado( acortando el array). 
-Por ejemplo, removerRango([20,30,40,50,60,70],2,4)debiera devolver [20,30,70].*/
-
 /*---------------Ejercicio numero 4--------------------------------------------------------------------------------*/
 
 function removerRango(x) {
 
     for (var i = 0; i < x.length; i++) {
-        if (x[i] = x[2]) { //malo
+        if (x[i] = x[2]) {
             z.pop(x[2]);
         }
     }
@@ -33,9 +32,7 @@ console.log(reemplazarNegativos([20, 30, 40, 50, 60, 70], 2, 4))
 
 /*---------------Ejercicio numero 2--------------------------------------------------------------------------------*/
 /*2-Dado un array, imprime los valores máximos (max), mínimos (min) y promedio (average) para el array. */
-/*1-Dados un array y un valor Y, cuenta e imprime (print) el número de valores del array que sean mayores que Y. 
 
-2-Dado un array, imprime los valores máximos (max), mínimos (min) y promedio (average) para el array. 
 function arrayMaxmin(x) {
 
 }
